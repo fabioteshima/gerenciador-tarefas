4f151e2e41c28c29918250a70724b498d0b44927
Fabio Teshima <fytrex@yahoo.com>
2026-01-18 10:04:09 -0300

    Renomeado GerenciadorDeTarefas para ListaDeTarefas

---
+++ src/entities/ListaDeTarefas.java
@@ -0,0 +1,28 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeTarefas {
+
+    private List< Tarefa> listaDeTarefas;
+
+    public ListaDeTarefas() {
+        this.listaDeTarefas = new ArrayList<>();
+    }
+
+    public List<Tarefa> getListaDeTarefas() {
+        return listaDeTarefas;
+    }
+
+    public void adicionarTarefa(Tarefa tarefa){
+        this.listaDeTarefas.add(tarefa);
+    }
+
+    @Override
+    public String toString() {
+        return "GerenciadorDeTarefas{" +
+                "listaDeTarefas=" + listaDeTarefas +
+                '}';
+    }
+}
